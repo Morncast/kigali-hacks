09a3609ec08e0f9933eacc0c0b22bc3a9ea327a5
brunoblaise <brunoblaise94@gmail.com>
2023-03-05 15:09:30 +0200

    fresh new look --not reponsive to all mobiles

--- app/head.tsx
+++ app/head.tsx
@@ -6,6 +6,10 @@ export default function Head() {
 			<meta charSet="utf-8" />
 			<link rel="icon" href="/favicon.ico" />
 			<meta name="title" content="MornCast Kigali Hacks Hackathon" />
+
+			<meta name="author" content="Morncast community"/>
+ <meta name="copyright" content="This is site and its content is copyright (c) 2023 of Morncast community"/>
+ <meta name="keywords" content="morncast, kigali, hackathon, hack, kigali hacks, kigali hackathon, kigali hack, kigali hacks hackathon "/>
 			<meta name="description" content="Kigali Hacks, Hack The Future.
 Join us at Kigali Hacks, in-person high school hackathon be a part of a 12 hour journey of coding, building, learning, and sharing. Open to all high schoolers across Rwanda, technical or non-technical backgrounds. We warmly welcome you"/>
 			<meta property="og:type" content="website" />
@@ -18,6 +22,7 @@ Join us at Kigali Hacks, in-person high school hackathon be a part of a 12 hour 
 			<meta property="twitter:title" content="MornCast Kigali Hacks Hackathon" />
 			<meta property="twitter:description" content="Kigali Hacks, Hack The Future.
 Join us at Kigali Hacks, in-person high school hackathon be a part of a 12 hour journey of coding, building, learning, and sharing. Open to all high schoolers across Rwanda, technical or non-technical backgrounds. We warmly welcome you"/>
+
 		</>
 	)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,55 @@
+'use client';
+
 import Image from 'next/image'
 import Link from 'next/link'
 
+import { useEffect, useState } from "react";
+
+
 function Home() {
+
+  
+    const [days, setDays] = useState(0);
+    const [hours, setHours] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+  
+    useEffect(() => {
+      const target = new Date("7/28/2023 23:59:59");
+  
+      const interval = setInterval(() => {
+        const now = new Date();
+        const difference = target.getTime() - now.getTime();
+  
+        const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+        setDays(d);
+  
+        const h = Math.floor(
+          (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        setHours(h);
+  
+        const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+        setMinutes(m);
+  
+        const s = Math.floor((difference % (1000 * 60)) / 1000);
+        setSeconds(s);
+  
+      }, 1000);
+  
+      return () => clearInterval(interval);
+    }, []);
+
+
 	return (
 		<div className='page'>
 			<header>
 				<nav>
 					<svg width="97" height="98" viewBox="0 0 97 98" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path fill-rule="evenodd" clip-rule="evenodd" d="M90.582 30.2962C97.1765 37.3722 98.6816 47.8383 94.3511 56.5055L86.9954 71.2277L79.0983 85.663C74.4492 94.1614 65.0236 98.8361 55.4947 97.3696L39.3089 94.8786L23.2269 91.7809C13.7591 89.9571 6.4287 82.3803 4.86997 72.8066L2.22234 56.545L0.18019 40.1951C-1.02207 30.5696 3.87303 21.2121 12.4386 16.7618L26.988 9.2025L41.8079 2.19551C50.5327 -1.92966 60.8885 -0.136051 67.741 6.68711L79.3807 18.2769L90.582 30.2962ZM49.9781 84.0758L48.6274 35.2576L72.9411 81.3636L49.9781 84.0758Z" fill="#0A0F0D" />
+						<path fillRule="evenodd" clipRule="evenodd" d="M90.582 30.2962C97.1765 37.3722 98.6816 47.8383 94.3511 56.5055L86.9954 71.2277L79.0983 85.663C74.4492 94.1614 65.0236 98.8361 55.4947 97.3696L39.3089 94.8786L23.2269 91.7809C13.7591 89.9571 6.4287 82.3803 4.86997 72.8066L2.22234 56.545L0.18019 40.1951C-1.02207 30.5696 3.87303 21.2121 12.4386 16.7618L26.988 9.2025L41.8079 2.19551C50.5327 -1.92966 60.8885 -0.136051 67.741 6.68711L79.3807 18.2769L90.582 30.2962ZM49.9781 84.0758L48.6274 35.2576L72.9411 81.3636L49.9781 84.0758Z" fill="#0A0F0D" />
 					</svg>
 					<div className='nav'>
+                    <li><Link href="#" target='_blank' className='button t1'>{days} {'d'} {hours} {'h'} {minutes} {'m'} {seconds} {'s'}</Link></li>
                     <li><Link href="https://bank.hackclub.com/donations/start/morncast" target='_blank' className='button'>Donate</Link></li>
 					<li><Link href="https://app.fillout.com/flow/ueioaUfgaxus" target='_blank'>Join us</Link></li>
 
@@ -19,7 +59,7 @@ function Home() {
 			<main>
 
 				<div>
-					<Image alt="" src="/humans.svg"   width='200' height='207' className='image2' />
+					<Image alt="" src="/humans.svg"   width='200' height='207' className='image2'  priority={true} />
 				</div>
 				<div className='main'>
 					<div
@@ -28,9 +68,203 @@ function Home() {
 					<div className='joinUs'>
 						Join us at Kigali Hacks, in-person high school hackathon be a part of a 12 hour journey of coding, building, learning, and sharing. Open to all high schoolers across Rwanda, technical or non-technical backgrounds. We warmly welcome you
 					</div>
+
+                    <div id="spots-container">
+                        <div className="blob green" aria-hidden="true"></div>
+                        <h2 id="header-subtitle-spots">284 spots remaining</h2>
+                    </div>
+
 				</div>
 			</main>
 			<section>
+
+
+          <div id='event-container'>
+	 <article id='event-description'>
+                    <h2 id="description-heading">Our Mission Statement</h2>
+                   
+                    <div id="event-main-box">
+                        <div id="description-content">Morncast Community is a student-led organization focused on promoting community engagement and improving TECH exposure for local high school students.</div>
+                      
+                        <ul className="list">
+                            <li>{"Even if you're not an expert at coding, this hackathon offers an opportunity for you to strengthen your skills and collaborate with others to create something useful. Below are important dates to keep in mind:"}
+                                <ul style={{marginLeft: '25px'}}>
+                                    <li><span className="emphasis-text">Jul 26</span> Signup Period Ends</li>
+                                    <li><span className="emphasis-text">Jul 28</span> 2023 Hackathon</li>
+                                </ul>
+                            </li>
+                        </ul>
+                        
+                    </div>
+                    <Image className='image5' width='100' height='207' alt="" src="/cat.png" />
+					<div id="timeline">
+                        <h2 id="timeline-heading">Timeline</h2>
+                        <span className="caption">Click on each dot for more info</span>
+                        <div className="flex-parent">
+                            <div className="input-flex-container">
+                                <input id="t1" className="timeline-input" type="radio" name="timeline-dot" data-description="1" defaultChecked/>
+                                <div onClick={()=> "check('t1')"} className="dot-info" data-description="1">
+                                    <span className="year">7/28 9:00am</span>
+                                    <span className="label">Opening Ceremony</span>
+                                </div>
+                                <input id="t2" className="timeline-input" type="radio" name="timeline-dot" data-description="2"/>
+                                <div onClick={()=> "check('t2')"} className="dot-info" data-description="2">
+                                    <span className="year">7/28 10:00am</span>
+                                    <span className="label">Hacking Begins</span>
+                                </div>
+                                <input id="t3" className="timeline-input" type="radio" name="timeline-dot" data-description="3"/>
+                                <div onClick={()=> "check('t3')"} className="dot-info" data-description="3">
+                                    <span className="year">7/28 8:00pm</span>
+                                    <span className="label">Projects Due</span>
+                                </div>
+                                <input id="t4" className="timeline-input" type="radio" name="timeline-dot" data-description="4"/>
+                                <div onClick={()=> "check('t4')"} className="dot-info" data-description="4">
+                                    <span className="year">7/28 9:00pm</span>
+                                    <span className="label">Project Presentations</span>
+                                </div>
+                                <input id="t5" className="timeline-input" type="radio" name="timeline-dot" data-description="5"/>
+                                <div onClick={()=> "check('t5')"} className="dot-info" data-description="5">
+                                    <span className="year">12/28 10:00pm</span>
+                                    <span className="label">Awards Ceremony</span>
+                                </div>
+                        
+                                <div id="timeline-descriptions-wrapper">
+                                    <div className="timeline-desc" data-description="1">
+                                        <div className="bracket" id="bracket-1">
+                                            <div className="left"></div>
+                                            <div className="right"></div>
+                                        </div>
+                                        <h3>Opening Ceremony</h3>
+                                        <h4 className="timeline-description-subtitle">7/28 | 9:0aam - 10:00am</h4>
+                                        <br/>
+                                        <p className="timeline-description">The official start of the hackathon where we deliver the opening presentation along with the finalized teams, prompts, rules, and timeline.
+                                            <ul className="timeline-description-list">
+                                                <li>It is highly recommended you attend this portion of the hackathon (at least half of your team must attend to participate)</li>
+                                                <li><span className="important-text">=IMPORTANT=</span> DO NOT START CODING during this period</li>
+                                            </ul>
+                                        </p>
+                                    </div>
+                                    <div className="timeline-desc" data-description="2">
+                                        <div className="bracket" id="bracket-2">
+                                            <div className="left"></div>
+                                            <div className="right"></div>
+                                        </div>
+                                        <h3>Hacking Begins</h3>
+                                        <h4 className="timeline-description-subtitle">7/28 | 10:00am</h4><br/>
+                                        <p className="timeline-description">
+                                         {"Now's your time to shine! Start working with your team to build your project. No strings attached!"}
+                                         <ul className="timeline-description-list">
+                                                <li><span className="important-text">=IMPORTANT=</span> Your project must follow one of the given prompts and guidelines; otherwise, your team may lose a lot of points for unrelated topic deductions</li>
+                                                <li>Remember use your time wisely</li>
+                                            </ul>
+                                        </p>
+                                    </div>
+                                    <div className="timeline-desc" data-description="3">
+                                        <div className="bracket" id="bracket-3">
+                                            <div className="left"></div>
+                                            <div className="right"></div>
+                                        </div>
+                                        <h3>Projects Due</h3>
+                                        <h4 className="timeline-description-subtitle">7/28 | 8:00pm</h4><br/>
+									    <p className="timeline-description">STOP coding! intense stress and turmoil are finally up! We hope you learned some valuable skills and made some new friends along the way.
+                                            <ul className="timeline-description-list">
+                                                <li><span className="important-text">=IMPORTANT=</span> Make sure you turn in your project BEFORE the deadline at 8:00pm. You can submit using our github
+                                                <Link href='https://github.com/Morncast' className="link" target='_blank'> Morncast Community</Link>
+                                                </li>
+                                                <li>{"Don't leave yet though, because you still need to present your project to the judges"}</li>
+                                                <li>From now until Project Presentations (8:30pm), your team will have around 30 mins to prepare for your presentation. Get back to work!</li>
+                                            </ul>
+                                        </p>
+                                    </div>
+                                    <div className="timeline-desc" data-description="4">
+                                        <div className="bracket" id="bracket-4">
+                                            <div className="left"></div>
+                                            <div className="right"></div>
+                                        </div>
+                                        <h3>Project Presentations</h3>
+                                        <h4 className="timeline-description-subtitle">7/28 | 8:30pm - 9:30pm</h4><br/>
+                                        <p className="timeline-description">This is where your team will present your product to our honored judges.<br/>
+                                            <ul className="timeline-description-list">
+                                                <li>Be Creative! Make your product stand out by including a demo or showing its function across multiple sources</li>
+                                                <li>Presentations will occur in order of team number (i.e. Team 1 will go first)</li>
+                                                <li>It is advised you have your camera on and that you ensure your microphone and tech work before presenting</li>
+                                                <li><span className="important-text">=IMPORTANT=</span>{"Your team's presentation is NOT ALLOWED to go above 5 minutes... Use your time wisely."}</li>
+                                                <li>Your team will be graded on both the product and presentation as per the rubric</li>
+                                            </ul>
+                                        </p>
+                                    </div>
+                                    <div className="timeline-desc" data-description="5">
+                                        <div className="bracket" id="bracket-5">
+                                            <div className="left"></div>
+                                            <div className="right"></div>
+                                        </div>
+                                        <h3>Awards Ceremony</h3>
+                                        <h4 className="timeline-description-subtitle">8/14 | 4:00pm - 4:30pm</h4><br/>
+                                        <p className="timeline-description">{"Congratulations! Judge scores are tallied and here's where we announce the 1st, 2nd, and 3rd place teams along with constructive feedback for each in the "}
+                                            <ul className="timeline-description-list">
+                                                <li><u>Winners</u>: we will contact you via Email about your prizes; allow up to 2 days for a response</li>
+                                            </ul>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+ </article>
+		  </div>
+  {/* <div id='schedule'>
+
+ 
+            
+    <div className='schedule-parent slide-animation'>
+  
+    <div className="schedule-element">
+    <div id="schedule-title" className="slide-animation">
+                <h2 id="schedule-heading">Event Schedule</h2>
+                <span className="caption">This is not the official 2023 Hackathon schedule, it might change.</span>
+            </div>
+                    <h3 className="time-column-header">Saturday</h3>
+                    <div className="schedule-container">
+                    
+                        <div className="time start-800"><span>8:00am</span></div>
+                        <div className="time start-900"><span>9:00am</span></div>
+                        <div className="time start-1000"><span>10:00am</span></div>
+                        <div className="time start-1100"><span>11:00am</span></div>
+                        <div className="time start-1200"><span>12:00pm</span></div>
+                        <div className="time start-1300"><span>1:00pm</span></div>
+                        <div className="time start-1400"><span>2:00pm</span></div>
+                        <div className="time start-1500"><span>3:00pm</span></div>
+                        <div className="time start-1600"><span>4:00pm</span></div>
+                        <div className="time start-1700"><span>5:00pm</span></div>
+                        <div className="time start-1800"><span>6:00pm</span></div>
+                        <div className="time start-1900"><span>7:00pm</span></div>
+                        <div className="time start-2000"><span>8:00pm</span></div>
+                        <div className="time start-2100"><span>9:00pm</span></div>
+                        
+                     
+                        <div className="event start-800 end-800 length-4 empty"></div>
+                        <div className="event logistics start-900 end-900 length-2">Check In<span>9 - 9:30am</span></div>
+                        <div className="event logistics start-900 end-900 length-2">Opening Ceremony<span>9:30 - 10am</span></div>
+                        <div className="event hacking start-1000 end-1000 length-4">✅ Hacking starts<span>Start 10am</span></div>
+                        <div className="event workshop start-1100 end-1100 length-4" >🌐 Web Dev Workshop<span>11am - 12pm</span></div>
+                        <div className="event food start-1200 end-1200 length-4" >🍕 Lunch<span>12 - 1pm</span></div>
+                        <div className="event start-1300 end-1300 length-4 empty"></div>
+                        <div className="event workshop start-1400 end-1400 length-4" >💻 Python Workshop<span>2 - 3pm</span></div>
+                        <div className="event start-1500 end-1500 length-4 empty"></div>
+                        <div className="event game start-1600 end-1600 length-4" >🏓 Ping Pong Tournament<span>Start 4pm</span></div>
+                        <div className="event game start-1700 end-1700 length-4">🤔 Kahoot (Programming Concepts)<span>Start 5pm</span></div>
+                        <div className="event start-1800 end-1800 length-4 empty"></div>
+                        <div className="event start-1900 end-1900 length-4 empty"></div>
+                        <div className="event start-2000 end-2000 length-4 empty"></div>
+                        <div className="event start-2100 end-2100 length-4 empty"></div>
+                    </div>
+                </div>
+    </div>
+
+  
+  </div> */}
+			<hr id="line-header"/>
 				<div className='sponsors'>
 					<div className='sponsors2'>Sponsors</div>
 
@@ -39,9 +273,7 @@ function Home() {
 							<Link href="https://hackclub.com/bank/" target="_blank">
 								<Image className='image4' width='100' height='207' alt="" src="/hcb-logo-original-dark.svg" />
 							</Link>
-							{/* <div>
-							<Image className='image5' width='100' height='207' alt="" src="/cat.png" />
-							</div> */}
+						
 						</div>
 
 						
