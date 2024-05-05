1d581dee458edc8614de9907eafeb1ec1e378280
Mudacumura Brunoblaise <brunoblaise94@gmail.com>
2024-05-05 09:54:33 +0200

    why section

--- app/page.tsx
+++ app/page.tsx
@@ -118,14 +118,14 @@ function Home() {
 				</div>
 			</main>
 			<section>
-				<div className="w-1/2 mx-auto text-center p-6 space-4">
+				<div className="w-1/2 mx-auto text-center p-[8rem] space-4">
 					<h2 className=" text-center kigaliHacksHack ">
 					Why you should join.
 						</h2>
 						<div className="text-center joinUs">
 						{"Why Get Involved? The reason why you shouldn't miss this."}
 					</div>
-					<div className="flex items-center flex-col justify-center gap-4 p-6 m-6 md:flex-row lg:flex-row ">
+					<div className="flex items-center flex-col justify-center gap-4 p-6 md:flex-row lg:flex-row ">
 						<div>
 							<CardStacker />
 						</div>
